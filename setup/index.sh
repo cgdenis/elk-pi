0543911f04384723b3f9c665f914febe6b5a220b
Carl <carl.g.denis@gmail.com>
2023-05-20 23:19:08 -0400

    Full instructions for Rpi4

--- setup/index.sh
+++ setup/index.sh
@@ -1,6 +1,10 @@
+
+#!/bin/bash
+
+#create elasticsearch mappings for tshark
 curl -X PUT "localhost:9200/_index_template/packets?pretty" -H 'Content-Type: application/json' -d'
 {
-   "index_patterns": "logstash-packets-*",
+   "index_patterns": "packets-*",
     "template": {
     "settings": {
       "number_of_shards": 1
@@ -58,6 +62,25 @@ curl -X PUT "localhost:9200/_index_template/packets?pretty" -H 'Content-Type: ap
         }
 
     }
+  }
+}
+'
+
+
+#create elasticsearch data view
+read -p "Enter username: " username
+
+# Prompt for password
+read -s -p "Enter password: " password
+echo
+
+data="username=$username&password=$password"
+
+curl -u $username:$password -X POST "localhost:5601/api/data_views/data_view" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
+{
+  "data_view": {
+     "title": "packets-*",
+     "name": "Packets Capture Data View"
   }
 }
 '
